619256c9a1ccaeb52a5056645dbdfeef1bfd1b52
Pavel Demin <pavel-demin@outlook.com>
2023-03-17 08:39:35 +0100

    move control socket to GUI thread in sdr-receiver-hpsdr.py

--- projects/sdr_receiver_hpsdr/server/sdr-receiver-hpsdr.py
+++ projects/sdr_receiver_hpsdr/server/sdr-receiver-hpsdr.py
@@ -18,18 +18,30 @@ Ui_Server, QMainWindow = loadUiType("sdr-receiver-hpsdr.ui")
 
 
 class Window(QMainWindow, Ui_Server):
+    bitstream = "sdr_receiver_hpsdr.bit"
+    # fmt: off
+    reply = [0xef, 0xfe, 0x02, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x19, 0x01, 0x52, 0x5f, 0x50, 0x49, 0x54, 0x41, 0x59, 0x41, 0x08]
+    # fmt: on
+    update_addr = Signal(object, int)
+    update_config = Signal(object)
+
     def __init__(self):
         super(Window, self).__init__()
         self.setupUi(self)
         # initialize variables
+        self.active = 0
+        self.socket = None
+        self.buffer = np.zeros(60, np.uint8)
+        self.buffer[:20] = self.reply
         self.thread = QThread()
         self.server = Server()
         self.server.moveToThread(self.thread)
         self.thread.started.connect(self.server.start)
         self.thread.finished.connect(self.server.stop)
-        self.server.started.connect(self.started)
         self.server.stopped.connect(self.stop)
-        self.server.print.connect(self.logViewer.appendPlainText)
+        self.server.print.connect(self.print)
+        self.update_addr.connect(self.server.update_addr)
+        self.update_config.connect(self.server.update_config)
         # create controls
         self.startButton.clicked.connect(self.start)
         for i in range(8):
@@ -39,37 +51,70 @@ class Window(QMainWindow, Ui_Server):
             input.addItems(["CH1", "CH2"])
             input.currentIndexChanged.connect(partial(self.server.update_input, i))
             self.inputLayout.addWidget(input, i * 2 + 1, 0)
+        # create JTAG
+        self.jtag = PyhubJTAG()
+
+    def print(self, text):
+        self.logViewer.appendPlainText(text)
 
     def start(self):
-        self.server.stopped.disconnect()
-        self.server.stopped.connect(self.stop)
-        self.thread.start()
-
-    def started(self):
+        try:
+            self.jtag.program(self.bitstream)
+        except:
+            self.print("error: %s" % sys.exc_info()[1])
+            return
+        self.print("FPGA configured")
+        time.sleep(0.1)
+        self.active = 0
+        self.socket = QUdpSocket(self)
+        self.socket.bind(1024)
+        self.socket.readyRead.connect(self.read_data)
         self.startButton.setText("Stop")
         self.startButton.clicked.disconnect()
         self.startButton.clicked.connect(self.stop)
+        self.print("server started")
 
     def stop(self):
-        self.server.stopped.disconnect()
-        self.server.stopped.connect(self.stopped)
+        if self.socket:
+            self.socket.close()
+            self.socket = None
         self.thread.quit()
         self.thread.wait()
-
-    def stopped(self):
         self.startButton.setText("Start")
         self.startButton.clicked.disconnect()
         self.startButton.clicked.connect(self.start)
+        self.print("server stopped")
 
     def closeEvent(self, event):
         self.stop()
 
+    def read_data(self):
+        datagram = self.socket.receiveDatagram()
+        addr = datagram.senderAddress()
+        port = datagram.senderPort()
+        data = np.frombuffer(datagram.data(), np.uint8)
+        if data.size < 4:
+            return
+        code = data[:4].view(np.uint32)[0]
+        if code == 0x0201FEEF:
+            self.update_config.emit(data[11:16])
+            self.update_config.emit(data[523:528])
+        elif code == 0x0002FEEF:
+            self.buffer[2] = 2 + self.active
+            self.socket.writeDatagram(self.buffer.tobytes(), addr, port)
+        elif code == 0x0004FEEF:
+            self.active = 0
+            self.thread.quit()
+            self.thread.wait()
+            self.print("client disconnected")
+        elif code in {0x0104FEEF, 0x0204FEEF, 0x0304FEEF}:
+            self.active = 1
+            self.thread.start()
+            self.update_addr.emit(addr, port)
+            self.print("client connected")
+
 
 class Server(QObject):
-    bitstream = "sdr_receiver_hpsdr.bit"
-    # fmt: off
-    reply = [0xef, 0xfe, 0x02, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x19, 0x01, 0x52, 0x5f, 0x50, 0x49, 0x54, 0x41, 0x59, 0x41, 0x08]
-    # fmt: on
     header = [
         [127, 127, 127, 0, 0, 33, 17, 25],
         [127, 127, 127, 8, 0, 0, 0, 0],
@@ -90,7 +135,6 @@ class Server(QObject):
         0x000500,
         0x002A00,
     ]
-    started = Signal()
     stopped = Signal()
     print = Signal(str)
 
@@ -103,15 +147,15 @@ class Server(QObject):
         self.socket = None
         self.addr = QHostAddress.Null
         self.port = 0
-        self.active = 0
         self.offset = 0
         self.receivers = 2
         self.config = np.zeros(10, np.uint32)
         self.status = np.zeros(1, np.uint32)
         self.rates = self.config[1:2].view(np.uint16)
         # create IO
-        self.jtag = PyhubJTAG()
         self.io = PyhubIO()
+        # create socket
+        self.socket = QUdpSocket(self)
         # create timers
         self.dataTimer = QTimer(self)
         self.dataTimer.timeout.connect(self.send_data)
@@ -120,14 +164,6 @@ class Server(QObject):
 
     def start(self):
         try:
-            self.jtag.program(self.bitstream)
-        except:
-            self.print.emit("error: %s" % sys.exc_info()[1])
-            self.stopped.emit()
-            return
-        self.print.emit("FPGA configured")
-        time.sleep(0.1)
-        try:
             self.io.start()
             self.io.flush()
             self.io.write(np.uint32(self.adc_cfg), 2, 0)
@@ -135,58 +171,21 @@ class Server(QObject):
             self.print.emit("error: %s" % sys.exc_info()[1])
             self.stopped.emit()
             return
-        self.socket = QUdpSocket(self)
-        self.socket.bind(1024)
-        self.socket.readyRead.connect(self.read_data)
-        self.print.emit("server started")
-        self.started.emit()
+        self.counter.fill(0)
+        self.offset = 0
+        self.receivers = 2
+        self.config.fill(0)
+        self.rates[0] = 2560
+        self.rates[1] = 2
+        self.send_ctrl()
+        self.reset_fifo()
+        self.dataTimer.start(1)
+        self.ctrlTimer.start(100)
 
     def stop(self):
-        self.active = 0
         self.dataTimer.stop()
         self.ctrlTimer.stop()
-        self.socket.close()
-        self.socket = None
         self.io.stop()
-        self.print.emit("server stopped")
-        self.stopped.emit()
-
-    def read_data(self):
-        datagram = self.socket.receiveDatagram()
-        addr = datagram.senderAddress()
-        port = datagram.senderPort()
-        data = np.frombuffer(datagram.data(), np.uint8)
-        if data.size < 4:
-            return
-        code = data[:4].view(np.uint32)[0]
-        if code == 0x0201FEEF:
-            self.update_config(data[11:16])
-            self.update_config(data[523:528])
-        elif code == 0x0002FEEF:
-            self.buffer.fill(0)
-            self.buffer[:20] = self.reply
-            self.buffer[2] = 2 + self.active
-            self.socket.writeDatagram(self.buffer[:60].tobytes(), addr, port)
-        elif code == 0x0004FEEF:
-            self.active = 0
-            self.dataTimer.stop()
-            self.ctrlTimer.stop()
-            self.print.emit("client disconnected")
-        elif code in {0x0104FEEF, 0x0204FEEF, 0x0304FEEF}:
-            self.counter.fill(0)
-            self.addr = addr
-            self.port = port
-            self.active = 1
-            self.offset = 0
-            self.receivers = 2
-            self.config.fill(0)
-            self.rates[0] = 2560
-            self.rates[1] = 2
-            self.send_ctrl()
-            self.reset_fifo()
-            self.dataTimer.start(1)
-            self.ctrlTimer.start(100)
-            self.print.emit("client connected")
 
     def update_config(self, data):
         code = data[0]
@@ -215,6 +214,10 @@ class Server(QObject):
             if self.config[9] != value:
                 self.config[9] = value
                 self.print.emit("RX8 frequency: %d Hz" % freq)
+
+    def update_addr(self, addr, port):
+        self.addr = addr
+        self.port = port
 
     def update_input(self, index, value):
         if value > 0:
