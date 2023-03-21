9a53e429a7036ec711287e7506286ccf5649b638
Pavel Demin <pavel-demin@outlook.com>
2023-03-21 22:00:04 +0100

    simplify led_blinker

--- projects/led_blinker/block_design.tcl
+++ projects/led_blinker/block_design.tcl
@@ -1,44 +1,8 @@
-# Create clk_wiz
-cell xilinx.com:ip:clk_wiz pll_0 {
-  PRIMITIVE PLL
-  PRIM_IN_FREQ.VALUE_SRC USER
-  PRIM_IN_FREQ 100.0
-  CLKOUT1_USED true
-  CLKOUT1_REQUESTED_OUT_FREQ 100.0
-  JITTER_SEL Min_O_Jitter
-  JITTER_OPTIONS PS
-  CLKIN1_UI_JITTER 600
-  USE_RESET false
-} {
-  clk_in1 adc_dco_i
-}
-
-# CDCE IIC
-
-# Create cdce_iic
-cell pavel-demin:user:cdce_iic iic_0 {
-  DATA_SIZE 132
-  DATA_FILE cdce_100.mem
-} {
-  iic cdce_iic_tri_io
-  aclk clk_i
-}
-
-# CDCE GPIO
-
-# Create cdce_gpio
-cell pavel-demin:user:cdce_gpio gpio_0 {} {
-  gpio cdce_gpio_tri_io
-  aclk clk_i
-}
-
-# LED
-
 # Create c_counter_binary
 cell xilinx.com:ip:c_counter_binary cntr_0 {
   OUTPUT_WIDTH 32
 } {
-  CLK pll_0/clk_out1
+  CLK clk_i
 }
 
 # Create port_slicer
