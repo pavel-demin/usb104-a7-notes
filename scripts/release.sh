8378a55feafc2fd47158b8da78a2e11a1c7b33f2
Pavel Demin <pavel-demin@outlook.com>
2023-03-22 07:49:02 +0100

    add notebooks and playground to release zip file

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,15 +4,14 @@ JOBS=`nproc 2> /dev/null || echo 1`
 
 make -j $JOBS cores
 
-make NAME=template xpr
-
-PRJS="mcpha sdr_receiver sdr_receiver_hpsdr"
+PRJS="led_blinker mcpha playground sdr_receiver sdr_receiver_hpsdr template"
 
 printf "%s\n" $PRJS | xargs -n 1 -P $JOBS -I {} make NAME={} bit
 
 tag=`date +%Y%m%d`
 
-mkdir -p $tag/usb104-a7-apps-win32 $tag/gnuradio $tag/template
+mkdir -p $tag/usb104-a7-apps-win32 $tag/gnuradio
+mkdir -p $tag/notebooks $tag/playground $tag/template
 
 dir=$tag/usb104-a7-apps-win32
 
@@ -37,6 +36,29 @@ cp tmp/sdr_receiver.bit $dir
 cp projects/sdr_receiver/gnuradio/fm_usb.grc $dir
 cp projects/sdr_receiver/gnuradio/fm_zmq.grc $dir
 
+dir=$tag/notebooks
+
+cp tmp/led_blinker.bit $dir
+cp tmp/playground.bit $dir
+cp tmp/template.bit $dir
+cp notebooks/01-led-blinker.ipynb $dir
+cp notebooks/02-playground.ipynb $dir
+cp notebooks/03-benchmarks.ipynb $dir
+cp notebooks/04-zmod-template.ipynb $dir
+cp notebooks/environment.yml $dir
+
+dir=$tag/playground
+
+cp -a cfg $dir
+cp -a tmp/cores $dir
+rm -rf $dir/cores/*.*
+cp -a cores/common_modules $dir/cores
+cp -a tmp/playground.gen $dir
+cp -a tmp/playground.ip_user_files $dir
+cp -a tmp/playground.srcs $dir
+cp tmp/playground.xpr $dir
+sed -i 's|Path=".*\.xpr"|Path="playground.xpr"|;s|\.\./||' $dir/playground.xpr
+
 dir=$tag/template
 
 cp -a cfg $dir
@@ -51,6 +73,6 @@ sed -i 's|Path=".*\.xpr"|Path="template.xpr"|;s|\.\./||' $dir/template.xpr
 
 cd $tag
 
-zip -r usb104-a7-notes-$tag.zip usb104-a7-apps-win32 gnuradio template
+zip -r usb104-a7-notes-$tag.zip usb104-a7-apps-win32 gnuradio notebooks playground template
 
 cd -
